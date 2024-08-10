7a836f88a3a0da3ea19ad83501522daf4a15e213
loveangelsa <155241488+loveangelsa@users.noreply.github.com>
2024-08-10 14:50:34 +0900

    Update run_standalone.sh

--- ezville_wallpad_Light/run_standalone.sh
+++ ezville_wallpad_Light/run_standalone.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ADDON_FILE=ezville_wallpad.py
+ADDON_FILE1=ezville_wallpad.py
+ADDON_FILE2=ezville_wallpad2.py
 OPTION_FILE=options_standalone.json
 GENERATE_OPTION_SCRIPT=generate_options_standalone.py
 
