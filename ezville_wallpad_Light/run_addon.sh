af025bd283c1b8cb178a8694e8ce8710768d1f7f
loveangelsa <155241488+loveangelsa@users.noreply.github.com>
2024-08-10 16:29:07 +0900

    Update run_addon.sh

--- ezville_wallpad_Light/run_addon.sh
+++ ezville_wallpad_Light/run_addon.sh
@@ -6,5 +6,5 @@ ADDON_FILE2=ezville_wallpad2.py
 #echo "[Info] run $ADDON_FILE1 ..."
 echo "[Info] run $ADDON_FILE2 ..."
 #python3 /srv/$ADDON_FILE1 "/data/options.json"
-python /srv/$ADDON_FILE2 "/data/options.json"
+python -u /$ADDON_FILE2
 echo "[Info] unexpected exit!"
