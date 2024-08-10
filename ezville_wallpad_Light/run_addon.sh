5abc17e17792eeecee1c103dd9eba23c8f5184a0
loveangelsa <155241488+loveangelsa@users.noreply.github.com>
2024-08-10 14:20:57 +0900

    Update run_addon.sh

--- ezville_wallpad_Light/run_addon.sh
+++ ezville_wallpad_Light/run_addon.sh
@@ -6,17 +6,8 @@ ADDON_FILE2=ezville_wallpad2.py
 # Execute the first Python script
 echo "[Info] Running $ADDON_FILE1 ..."
 python3 $ADDON_FILE1
+echo "[Info] $ADDON_FILE1 exited unexpectedly!"
 
-# Check if the first script exited unexpectedly
-if [ $? -ne 0 ]; then
-    echo "[Info] $ADDON_FILE1 exited unexpectedly!"
-fi
-
-# Execute the second Python script
 echo "[Info] Running $ADDON_FILE2 ..."
 python3 $ADDON_FILE2
-
-# Check if the second script exited unexpectedly
-if [ $? -ne 0 ]; then
-    echo "[Info] $ADDON_FILE2 exited unexpectedly!"
-fi
+echo "[Info] $ADDON_FILE2 exited unexpectedly!"
