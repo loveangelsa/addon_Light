f81541aeaac86e82a35944a235b36d0b36600dc7
loveangelsa <155241488+loveangelsa@users.noreply.github.com>
2024-08-10 12:40:24 +0900

    run_addon.sh

--- ezville_wallpad_Light/run_addon.sh
+++ ezville_wallpad_Light/run_addon.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ADDON_FILE=ezville_wallpad.py
+ADDON_FILE1=ezville_wallpad.py
+ADDON_FILE2=ezville_wallpad2.py
 
 echo "[Info] run $ADDON_FILE ..."
 python3 /srv/$ADDON_FILE "/data/options.json"
